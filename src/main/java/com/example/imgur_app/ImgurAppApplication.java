f3c6777c9bad6d84fa11010a27b4322fc3f93daa
Nick Sugiarto <nasugiarto@gmail.com>
2025-01-16 15:00:13 -0800

    Initial commit for Spring Boot Imgur project

---
+++ src/main/java/com/example/imgur_app/ImgurAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.imgur_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgurAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgurAppApplication.class, args);
+	}
+
+}
